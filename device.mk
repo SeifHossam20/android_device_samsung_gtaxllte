1919dd4c8bae0faeb70b369007541a0badce53de
K9100ii <k9100ii.ki@gmail.com>
2024-05-21 14:41:09 +0100

    gtaxllte: Declare support for ConnectionService API
    See gtaxl-common commit titled 'gtaxl-common: Switch
    handheld_core_hardware permissions file to tablet variant'.
    
    Change-Id: I029e475a4fa5b2952a4339994630939604ac4340

--- device.mk
+++ device.mk
@@ -30,6 +30,7 @@ DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.connectionservice.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.connectionservice.xml \
     frameworks/native/data/etc/android.software.sip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
